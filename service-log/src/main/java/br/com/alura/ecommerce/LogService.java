afb8bda7795dbf8eaf5884a9d692ee7e5eb9ab4c
Dev-JeanSantos <jeancbsan@gmail.com>
2021-04-14 11:41:32 -0300

    Microservices as modules in a monorepo

--- service-log/src/main/java/br/com/alura/ecommerce/LogService.java
+++ service-log/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -10,10 +10,9 @@ import java.util.regex.Pattern;
 public class LogService {
 
     public static void main(String[] args) {
-
         var logService = new LogService();
-        try (var service = new KafkaService<>(LogService.class.getSimpleName(),
-                Pattern.compile("ECOMMERCE.*"),
+        try (var service = new KafkaService(LogService.class.getSimpleName(),
+                Pattern.compile("ECOMERCE.*"),
                 logService::parse,
                 String.class,
                 Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
@@ -22,8 +21,8 @@ public class LogService {
     }
 
     private void parse(ConsumerRecord<String, String> record) {
-        System.out.println("---------------------------------------");
-        System.out.println("LOG - " + record.topic() );
+        System.out.println("------------------------------------------");
+        System.out.println("LOG: " + record.topic());
         System.out.println(record.key());
         System.out.println(record.value());
         System.out.println(record.partition());
